9aadf859c40998ca7299041b1629e6116d454264
David Elias <36795720+davidelias346@users.noreply.github.com>
2022-01-23 22:54:22 -0300

    Cadastro de séries (in memory)

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using Series.Interfaces;
+
+namespace Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+
+        private List<Serie> listaSerie = new List<Serie>();
+
+       public List<Serie> Lista()
+       {
+           return listaSerie;
+
+       }
+       
+       
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Series 
+{
+    internal class Program
+    {   
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                   case "1":
+                        ListarSerie();
+						break;
+					case "2":
+						InserirSerie();
+						break;
+					case "3":
+						AtualizarSerie();
+						break;
+					case "4":
+						ExcluirSerie();
+						break;
+					case "5":
+						VisualizarSerie();
+						break;
+					case "C":
+						Console.Clear();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException(); 
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static void ListarSerie() 
+        {   
+            System.Console.WriteLine("Listar séries\n");
+
+            var lista = repositorio.Lista();
+
+            if(lista.Count == 0)
+            {
+                System.Console.WriteLine("Nenhuma série foi cadastrada!");
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.RetornaExcluido();
+
+                System.Console.WriteLine($"Id: {serie.RetornaId()} Nome: {serie.RetornaTitulo()}  Item excluído: {(excluido ? "Sim" : "Não")}"); //if ternário
+            }
+            
+
+        }
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série");
+
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o número do gênero correspondente: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título da Série: ");
+			string entradaTitulo = Console.ReadLine();
+
+			Console.Write("Digite o Ano de Início da Série: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição da Série: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Insere(novaSerie);
+        }
+
+        private static void AtualizarSerie()
+        {
+            Console.WriteLine("Digite o id da série que deseja atualizar: ");
+            int entradaId = int.Parse(Console.ReadLine());
+
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o número do gênero correspondente: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título da Série: ");
+			string entradaTitulo = Console.ReadLine();
+
+			Console.Write("Digite o Ano de Início da Série: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição da Série: ");
+			string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: entradaId,
+										genero: (Genero)entradaGenero,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Atualiza(entradaId, atualizaSerie);
+        }
+
+        private static void ExcluirSerie()
+        {
+            Console.WriteLine("Digite o id da série que deseja excluir: ");
+            int entradaId = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(entradaId);
+        }
+
+        private static void VisualizarSerie()
+        {
+            Console.WriteLine("Digite o id da série que deseja visualizar");
+            int entradaId = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(entradaId);
+            Console.WriteLine(serie);
+        }
+
+        private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Seja bem-vindo(a) ao MyFlix, seu gerenciador de séries");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar séries");
+			Console.WriteLine("2- Inserir nova série");
+			Console.WriteLine("3- Atualizar série");
+			Console.WriteLine("4- Excluir série");
+			Console.WriteLine("5- Visualizar série");
+			Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+
+
+    }
+}
